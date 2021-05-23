833e713bb2943399a1f3ccb0ece9685bf5baae8a
austinald1 <81729807+austinald1@users.noreply.github.com>
2021-05-23 12:41:38 -0700

    added array to generate password

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ var specialCharacters = ['@','+','\\','/',"'",'!','#','$','^','?',':',',',')','(
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];// 
 var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var length;
 var specialCharactersFlag;
 var numbersFlag;
 var capitalLettersFlag;
 var lowerCaseFlag;
 //Write password to the #password input
 function getOptions(){
-  var length = prompt ("How long do you want your password to be?");
+  length = prompt ("How long do you want your password to be?");
   console.log(length);
   //create another if statement beyond 128, 
   if (length<8) {
@@ -25,7 +26,7 @@ function getOptions(){
   }
   var lowerCasePrompt = prompt("Would you like lowercase characters in your password.Please respond with yes or no(case sensitive)");
   if (lowerCasePrompt == "yes"){
-   lowerCaseFlag = true;
+    lowerCaseFlag = true;
   } else if (lowerCasePrompt == "no"){
     lowerCaseFlag = false;
   }else {
@@ -55,6 +56,9 @@ function getOptions(){
   }else {
     return null;
   }
+  if (!specialCharactersFlag && !numbersFlag && !capitalLettersFlag && !lowerCaseFlag){
+    alert ("atleast one special character should be selected")
+  }
 } 
 
 
@@ -62,7 +66,11 @@ function getOptions(){
 //use confirm("")
 function generatePassword (){
   let options = getOptions();
-  
+   var results = [];
+   var possibleCharacters = [];
+   var mandatoryCharacters = [];
+   if (!options)return null;
+   
 }
 
 
